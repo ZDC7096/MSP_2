ddb92b1e78849506ff0802a6801e25e50c588970
Didac Fernandez <didac.fernandez@insiemecloud.net>
2023-01-26 21:26:26 -0800

    clean up app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import axios from 'axios';
-import {useEffect, useState} from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 import Search from './components/Search'
 import PlaylistView from './components/PlaylistView';
 import DisplaySongs from './components/DisplaySongs';
@@ -11,25 +8,11 @@ import NavBar from './components/NavBar'
 
 function App() {
   return(
-<>
-<NavBar/>
- {/* <Router>
-
-<Routes>  
-          <Route path="/" element={<NavBar/>}/>
-          <Route path="/search" element={<Search />} />
-          <Route path="/playlistview" element={<PlaylistView />} />
-        </Routes>
-     </Router> */}
-</>
-
-
-
-
+    <>
+      <NavBar/>
+    </>
   )
 }
     
-
-
 export default App;
 
