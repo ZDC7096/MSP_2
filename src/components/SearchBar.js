6a84427cd411979d14a5b1b5f9090913113fb9bb
Jkishi6 <jonathankishi6@gmail.com>
2023-01-26 17:13:03 -0800

    made song display look better

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,20 @@
 import { useState } from 'react'
+import { TextField, Button } from '@mui/material'
 
 function SearchBar(props) {
     let [searchTerm, setSearchTerm] = useState('')
+    
 
     return (
         <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
 
-            <input type="text" placeholder="Enter a search term here" onChange={
-                (e) => setSearchTerm(e.target.value)
-            }/>
+            <TextField id="outlined-basic" label="Search" variant="outlined" type="text" 
+                sx={{height:"50px"}}
+                onChange={
+                    (e) => setSearchTerm(e.target.value)
+                } />
 
-            <input type="submit" />
+            <Button variant="contained" type="submit" sx={{height: "50px"}}>Submit</Button>
 
         </form>
     )
